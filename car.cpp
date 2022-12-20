2762851159b92574f4689ba3b1fb903197324b1b
awais1019 <120665004+awais1019@users.noreply.github.com>
2022-12-20 09:44:46 +0500

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"                         --------------------------        "<<endl;
+cout<<"                      -    |          |              -        "<<endl;
+cout<<"                    -      |          |                -         "<<endl;
+cout<<"                  -        |          |                  -          "<<endl;
+cout<<"                - --------------------- ---------------    -  ------------------    "<<endl;        
+cout<<"               -                                                                |      "<<endl;
+cout<<"                -                                                               |       "<<endl;
+cout<<"                 -                                                              |         "<<endl;
+cout<<"                  - ________         ___________________________           _____|   "<<endl;
+cout<<"                            |  (o)  |                            |  (o)  |             "<<endl;
+cout<<"                            | _____ |                            | _____ |                "<<endl;}
